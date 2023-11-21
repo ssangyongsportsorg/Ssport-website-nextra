426a30ef543fdad2fdd54c2336b63264720fd83e
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-21 21:40:37 +0800

    Delete app directory

--- app/i18n.ts
+++
@@ -1,1 +0,0 @@
-export const defaultLanguage = 'zh-tw'
--- app/source.ts
+++
@@ -1,7 +0,0 @@
-import { allDocs, allMeta } from 'contentlayer/generated'
-import { createContentlayer } from 'next-docs-zeta/contentlayer'
- 
-export const { getPage, getPageUrl, tree } = createContentlayer(
-  allMeta,
-  allDocs
-)
