8ef78ac69412d3ecfd353ac17da41686ba218e87
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-21 21:41:49 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const { withContentlayer } = require("next-contentlayer");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-  appDir: true,
-  serverComponentsExternalPackages: ["mongoose"],
-  },
-};
-
-module.exports = withContentlayer(nextConfig);
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx'
+})
+ 
+module.exports = withNextra()
+ 
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
