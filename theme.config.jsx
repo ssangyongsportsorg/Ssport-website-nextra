8e97f102e694cfdbe16ea5c5eb8f01d163ec6a29
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-21 23:15:11 +0800

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,7 @@
 import Footer from './pages/components/footer'
-
+import DocSearch from './pagescomponents/docsearch'
 export default {
+  customSearch: <DocSearch/>,
   logo: <span>My Nextra Documentation</span>,
   project: {
     link: 'https://github.com/shuding/nextra'
