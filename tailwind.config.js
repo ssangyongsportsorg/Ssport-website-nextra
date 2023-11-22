c83ad5d9721950e6122bfcafb23df3c34f84437f
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-23 00:35:46 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   content: [
     "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{ts,tsx}"
-    "./pages/**/*.{md,mdx}",
+    "./pages/**/*.{md,mdx}"
     "./public/**/*.html",
   ],
  plugins: [
