bbfe1ff2d531cb79df29407e75739439ac6b99e4
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-22 17:58:13 +0800

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,5 @@
 import Footer from './pages/components/footer'
-import DocSearch from './pagescomponents/docsearch'
+import DocSearch from './components/docsearch'
 export default {
   customSearch: <DocSearch/>,
   logo: <span>My Nextra Documentation</span>,
