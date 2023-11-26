2402b274a8397aba33a0d0709ab2d30d72598f8d
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-26 21:55:47 +0800

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -31,15 +31,9 @@ export default {
       "雙龍體育網站- 專業體育賽事！ 提供最新的雙龍體育資訊、TV、運動比賽報導，讓您輕鬆了解雙龍體育。歡迎加入雙龍運動論壇，一起與大家溝通！";
 
     return {
-      canonical: `https://yeecord.com${asPath}`,
+      canonical: `https://ssangyongports.eu.org${asPath}`,
       titleTemplate: route === "/" ? "%s" : "%s – 雙龍體育",
-      twitter: {
-        cardType: "summary_large_image",
-      },
-      description: description,
-      openGraph: {
-        description: description,
-        type: "website",
+
         images: [
           image || {
             url: "/seo.png",
