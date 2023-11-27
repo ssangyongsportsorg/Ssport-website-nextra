eb6da01e9d6c06f2edf9fa0902e7819acd8f3cf1
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-27 21:30:41 +0800

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -17,31 +17,49 @@ export default {
    search: {
     placeholder: "搜尋....",
   },
-   useNextSeoProps() {
-    const { asPath, route } = useRouter();
-    const { frontMatter, title } = useConfig();
+  head: () => {
+    const { asPath, pathname } = useRouter();
+    const { frontMatter } = useConfig();
 
-    const image = frontMatter.image != null && {
-      alt: title,
-      url: frontMatter.image,
+    const ogConfig = {
+      title: '雙龍體育',
+      description: '雙龍體育網站- 專業體育賽事！ 提供最新的雙龍體育資訊、TV、運動比賽報導，讓您輕鬆了解雙龍體育。歡迎加入雙龍運動論壇，一起與大家溝通！',
+      favicon: '/favicon.svg',
     };
+    const favicon = String(ogConfig.favicon);
+    const title = String(frontMatter.title || ogConfig.title);
+    const description = String(frontMatter.description || ogConfig.description);
+    const note =
+      (frontMatter.date as string | undefined) ?? pathname === '/'
+        ? 'million.dev'
+        : pathname;
+    const canonical = new URL(asPath, 'https://ssangyongsports.eu.org').toString();
 
-    const description =
-      frontMatter.description ??
-      "雙龍體育網站- 專業體育賽事！ 提供最新的雙龍體育資訊、TV、運動比賽報導，讓您輕鬆了解雙龍體育。歡迎加入雙龍運動論壇，一起與大家溝通！";
+    const ogUrl =
+      pathname === '/'
+        ? `/seo.png`
+        : `/seo.png`;
+    
+    return (
+      <>
+        <meta property="og:url" content={canonical} />
+        <link rel="canonical" href={canonical} />
 
-    return {
-      canonical: `https://yeecord.com${asPath}`,
-      titleTemplate: route === "/" ? "%s" : "%s – 雙龍體育",
-        images: [
-          image || {
-            url: "/seo.png",
-            alt: "雙龍體育",
-          },
-        ],
-      },
-    };
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+    
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="og:image" content={ogUrl} />
+
+        <link rel="shortcut icon" href={favicon} type="image/svg+xml" />
+        <link rel="apple-touch-icon" href={favicon} type="image/svg+xml" />
+        <meta name="apple-mobile-web-app-title" content={title} />
+
+      </>
+    );
   },
+   
    navbar: {
     extraContent: (
       <div className="flex flex-row gap-3">
